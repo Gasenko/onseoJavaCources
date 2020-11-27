e52b4595d67bff36ab839363b2f39c43276b08bc
Denys Gasenko <denisgasenko@gmail.com>
2020-11-27 17:52:16 +0200

    Add Cylinder

--- ClassWork/src/com/course/onseo/Homework/Cylinder.java
+++ ClassWork/src/com/course/onseo/Homework/Cylinder.java
@@ -1,4 +1,44 @@
 package com.course.onseo.Homework;
 
-public class Cylinder {
+public class Cylinder extends Circle{
+    protected double height;
+
+    public Cylinder()
+    {
+        setHeight(0);
+    }
+
+    public Cylinder(double h, double r, int a, int b)
+    {
+        super(r,a,b);
+        setHeight(h);
+    }
+
+    public void setHeight(double h)
+    {
+        if (h >= 0)
+            height = h;
+        else
+            height = 0;
+    }
+
+    public double getHeight()
+    {
+        return height;
+    }
+
+    public double area()
+    {
+        return 2 * super.area() + (2 * Math.PI * radius * height);
+    }
+
+    public double volume()
+    {
+        return super.area() * height;
+    }
+
+    public String toString()
+    {
+        return super.toString() + "; Height = " + height;
+    }
 }
