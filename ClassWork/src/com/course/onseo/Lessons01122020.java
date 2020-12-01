82b43901c8eb6102f03f1a2b84e16a949659cffe
Denys Gasenko <Denys.Gasenko@onseo.biz>
2020-12-01 18:44:21 +0200

    remove trash

---
+++ ClassWork/src/com/course/onseo/Lessons01122020.java
@@ -0,0 +1,15 @@
+package com.course.onseo;
+
+import com.course.onseo.Annimal.Dog;
+
+public class Lessons01122020 extends Dog {
+
+    public Lessons01122020(String name, String type, int age) {
+        super(name, type, age);
+    }
+
+    public void say(){
+        System.out.println("bla bla");
+    }
+
+}
--- JavaRush/src/com/My/JavaRush/level_4_7/Exercise05.java
+++ JavaRush/src/com/My/JavaRush/level_4_7/Exercise05.java
@@ -1,4 +1,17 @@
 package com.My.JavaRush.level_4_7;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Exercise05 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+
+
+
+    }
 }
