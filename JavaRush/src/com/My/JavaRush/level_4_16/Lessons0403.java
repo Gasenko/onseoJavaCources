cb58c4325a2c010874e806ee68212ca45b3da700
Denys Gasenko <Denys.Gasenko@onseo.biz>
2020-12-11 00:30:20 +0200

    JavaRush level 4 is DONE!

--- JavaRush/src/com/My/JavaRush/level_4_16/Lessons0403.java
+++ JavaRush/src/com/My/JavaRush/level_4_16/Lessons0403.java
@@ -1,15 +1,31 @@
 package com.My.JavaRush.level_4_16;
 
-public class Lessons0403 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Lessons0403 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int sum = 0;
+        for(int i = 0; ; i++)
+
+        {
+            int a = Integer.parseInt(reader.readLine());
+            sum = a + sum;
+
+            if(a == -1)
+            {
+                System.out.println(sum);
+                break;
+            }
+        }
     }
 }
 /*
 Вводить с клавиатуры числа.
 Если пользователь ввел -1, вывести на экран сумму всех введенных чисел и завершить программу.
 -1 должно учитываться в сумме.
-
 Подсказка: один из вариантов решения этой задачи, использовать конструкцию:
 
 while (true) {
