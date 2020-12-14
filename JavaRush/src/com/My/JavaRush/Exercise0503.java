3d948acee79e4d8d6d0f65270d5225ad77ffb1e0
Denis Gasenko <57732922+Gasenko@users.noreply.github.com>
2020-12-14 16:55:51 +0200

    Update Exercise0503.java

--- JavaRush/src/com/My/JavaRush/Exercise0503.java
+++ JavaRush/src/com/My/JavaRush/Exercise0503.java
@@ -1,1 +1,29 @@
+/*
+    String name;
+    int age;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+Создать class Dog. У собаки должна быть кличка String name и возраст int age.
+Создай геттеры и сеттеры для всех переменных класса Dog.
+Требования:
+- програма не должна считывать данные с клавиатуры;
+- у класса Dog должна быть переменная name с типом String;
+- у класса Dog должна быть переменная age с типом int;
+- у переменных должны быть геттери и сетери.
+*/
