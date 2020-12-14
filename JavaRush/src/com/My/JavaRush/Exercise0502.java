f0ff05280b11ad3baf049905b919c7902445550e
Denis Gasenko <57732922+Gasenko@users.noreply.github.com>
2020-12-14 18:01:15 +0200

    Update Exercise0502.java

--- JavaRush/src/com/My/JavaRush/Exercise0502.java
+++ JavaRush/src/com/My/JavaRush/Exercise0502.java
@@ -1,5 +1,31 @@
-
 /*
+Реализовать метод fight                                                  
+                                               
+                                                  
+public class Cat {                                                  
+    public int age;                                                  
+    public int weight;                                                  
+    public int strength;                                                  
+                                                  
+    public Cat() {                                                  
+    }                                                  
+                                                  
+    public boolean fight(Cat anotherCat) {                                                  
+        //напишите тут ваш код
+                   int ageScore = Integer.compare(this.age, anotherCat.age);
+                int weightScore = Integer.compare(this.weight, anotherCat.weight);
+                int strengthScore = Integer.compare(this.strength, anotherCat.strength);
+
+                int score = ageScore + weightScore + strengthScore;
+                return score > 0; // return score > 0 ? true : false;                                               
+    }                                                  
+                                                  
+    public static void main(String[] args) {                                                  
+                                                  
+    }                                                  
+}                            
+
+
 Реализуй метод boolean fight(Cat anotherCat):
 продумай механизм драки котов в зависимости от их веса, возраста и силы.
 Сравни каждый критерий по отдельности, чтобы победителем был тот, у кого были лучшие показатели по большинству критериев.
