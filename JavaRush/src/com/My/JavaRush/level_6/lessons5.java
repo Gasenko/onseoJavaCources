507b20b79d66e39dfb750af37ae54e737a32e933
Denys Gasenko <Denys.Gasenko@onseo.biz>
2020-12-28 00:34:17 +0200

    JavaRush level 6 inprogress!

---
+++ JavaRush/src/com/My/JavaRush/level_6/lessons5.java
@@ -0,0 +1,27 @@
+package com.My.JavaRush.level_6;
+
+public class lessons5 {
+
+}
+/*
+В каждом классе Cat и Dog написать метод finalize, который выводит на экран текст о том,
+что такой-то объект уничтожен.
+
+ */
+
+
+/*
+Метод finalize класса Cat
+
+public class Cat {
+    //напишите тут ваш код
+    protected void finalize() throws Throwable{
+    }
+    public static void main(String[] args) {
+
+    }
+}
+ */
+/*
+В классе Cat создать метод protected void finalize() throws Throwable.
+ */
